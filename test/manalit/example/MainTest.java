69517ecc0109724614eb7c2e749d57e568b7d2f2
Manali <manu@RLB.com>
2014-01-25 16:03:14 +0530

    2st commit

---
+++ test/manalit/example/MainTest.java
@@ -0,0 +1,17 @@
+package manalit.example;
+
+import org.junit.Test;
+import static junit.framework.Assert.assertEquals;
+
+/**
+ * Created by manalit on 1/25/14.
+ */
+public class MainTest {
+    @Test
+    public void testMain() throws Exception {
+        int expected = 11;
+        int actual = 11;
+        assertEquals(actual,expected);
+
+    }
+}
